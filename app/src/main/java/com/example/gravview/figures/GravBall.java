46e3878bb817e33f86afdc7bd957396c5f39fa3d
115MRLI <1159642490@qq.com>
2019-05-06 13:08:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gravview/figures/GravBall.java
@@ -0,0 +1,25 @@
+package com.example.gravview.figures;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+/**
+ * 球形粒子基类
+ */
+public class GravBall extends Grav {
+    private int radius;
+
+    public GravBall(PointF startPoint, Paint paint, int radius) {
+        super(startPoint, paint);
+        this.radius = radius;
+    }
+
+    public void draw(Canvas canvas, PointF drawPoint) {
+        canvas.drawCircle(drawPoint.x, drawPoint.y, radius, paint);
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
